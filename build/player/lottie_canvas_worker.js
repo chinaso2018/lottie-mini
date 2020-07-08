b1977b6ddef11516475ae20731a5584ccc3c5724
zhouyuying <zhouyuying@chinaso.com>
2020-07-08 19:05:32 +0800

    fix trigger bug

--- build/player/lottie_canvas_worker.js
+++ build/player/lottie_canvas_worker.js
@@ -235,45 +235,53 @@ var rgbToHex = (function(){
         return '#' + colorMap[r] + colorMap[g] + colorMap[b];
     };
 }());
-function BaseEvent(){}
+/*
+ * @Author: zhouyuying
+ * @Date:   2020-07-08 17:49:26
+ * @Last Modified by:   zhouyuying
+ * @Last Modified time: 2020-07-08 17:49:26
+ */
+function BaseEvent() {}
 BaseEvent.prototype = {
-	triggerEvent: function (eventName, args) {
-	    if (this._cbs[eventName]) {
-	        var len = this._cbs[eventName].length;
-	        for (var i = 0; i < len; i++){
-	            this._cbs[eventName][i](args);
-	        }
-	    }
-	},
-	addEventListener: function (eventName, callback) {
-	    if (!this._cbs[eventName]){
-	        this._cbs[eventName] = [];
-	    }
-	    this._cbs[eventName].push(callback);
+  triggerEvent: function (eventName, args) {
+    if (this._cbs && this._cbs[eventName]) {
+      var len = this._cbs[eventName].length
+      for (var i = 0; i < len; i++) {
+        this._cbs[eventName][i](args)
+      }
+    }
+  },
+  addEventListener: function (eventName, callback) {
+    if (!this._cbs[eventName]) {
+      this._cbs[eventName] = []
+    }
+    this._cbs[eventName].push(callback)
 
-		return function() {
-			this.removeEventListener(eventName, callback);
-		}.bind(this);
-	},
-	removeEventListener: function (eventName,callback){
-	    if (!callback){
-	        this._cbs[eventName] = null;
-	    }else if(this._cbs[eventName]){
-	        var i = 0, len = this._cbs[eventName].length;
-	        while(i<len){
-	            if(this._cbs[eventName][i] === callback){
-	                this._cbs[eventName].splice(i,1);
-	                i -=1;
-	                len -= 1;
-	            }
-	            i += 1;
-	        }
-	        if(!this._cbs[eventName].length){
-	            this._cbs[eventName] = null;
-	        }
-	    }
-	}
-};
+    return function () {
+      this.removeEventListener(eventName, callback)
+    }.bind(this)
+  },
+  removeEventListener: function (eventName, callback) {
+    if (!callback) {
+      this._cbs[eventName] = null
+    } else if (this._cbs[eventName]) {
+      var i = 0,
+        len = this._cbs[eventName].length
+      while (i < len) {
+        if (this._cbs[eventName][i] === callback) {
+          this._cbs[eventName].splice(i, 1)
+          i -= 1
+          len -= 1
+        }
+        i += 1
+      }
+      if (!this._cbs[eventName].length) {
+        this._cbs[eventName] = null
+      }
+    }
+  },
+}
+
 var createTypedArray = (function(){
 	function createRegularArray(type, len){
 		var i = 0, arr = [], value;
